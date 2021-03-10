c351833cd0225ccae9b88dd32fc4cf91d7045f20
Summer <72228259+SummerinTech@users.noreply.github.com>
2021-03-09 20:19:45 -0800

    shorten New York

--- src/Weather.js
+++ src/Weather.js
@@ -5,7 +5,7 @@ import "./Weather.css";
 export default function Weather() {
 	return (
 		<div className="Weather">
-			<h1>New York, NY</h1>
+			<h1>New York</h1>
 			<ul>
 				<li> Wednesday 7:00</li>
 				<li>Mostly cloudy</li>
