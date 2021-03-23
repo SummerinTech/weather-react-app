322e1fd1ec579c1afe13efa0e31636cd1399d2ee
Summer <72228259+SummerinTech@users.noreply.github.com>
2021-03-23 16:14:27 -0700

    adjust app spacing and layout

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,24 +3,25 @@ import FormattedDate from "./FormattedDate";
 import ReactAnimatedWeather from "react-animated-weather";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
+import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
 	return (
 		<div className="weatherInfo">
 			<h1>{props.data.city}</h1>
-			<ul>
+
+			<ul className="LeftUl">
 				<li>
 					<FormattedDate date={props.data.date} />
 				</li>
-				<li>{props.data.description}</li>
+				<li className="description">{props.data.description}</li>
 			</ul>
-
 			<div className="row">
-				<div className="col-6">
+				<div className="col-6 weather-group">
 					<WeatherIcon code={props.data.icon} />
 					<WeatherTemperature celsius={props.data.temperature} />
 				</div>
-				<div className="col-6">
+				<div className="col-6 data-group">
 					<ul>
 						<li>Humidity: {props.data.humidity}%</li>
 						<li>Wind Speed: {props.data.wind} km/h</li>
